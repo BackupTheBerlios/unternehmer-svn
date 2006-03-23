89bbe3ecdee8af8a3d324fbd35e96bc3aea2b1b2
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-03-23 21:43:01 +0000

    kunde erfassen angelegt1

---
+++ branches/flo/kunde_erfassen.php
@@ -0,0 +1,157 @@
+<table width="100%" border="1">
+<tr>
+	<td colspan="2"><h2><center>Kunde erfassen<center></h2></td>
+</tr>
+<tr>&nbsp;</tr>
+<tr>
+	<td colspan="2"><h3><center>Rechnungsadresse<center></h3></td>
+</tr>
+<tr>
+	<td>Firmenname</td>
+	<td><input type="text" name="firmenname"></td>
+</tr>
+<tr>
+	<td>Vorname</td>
+	<td><input type="text" name="vorname"></td>
+</tr>
+<tr>
+	<td>Nachname</td>
+	<td><input type="text" name="nachname"></td>
+</tr>
+<tr>
+	<td>Strasse</td>
+	<td><input type="text" name="strasse"></td>
+</tr>
+<tr>
+	<td>PLZ</td>
+	<td><input type="text" name="plz"></td>
+</tr>
+<tr>
+	<td>Stadt</td>
+	<td><input type="text" name="stadt"></td>
+</tr>
+<tr>
+	<td>Land</td>
+	<td><input type="text" name=""></td>
+</tr>
+<tr>
+	<td colspan="2"><h3><center>Lieferadresse</center></h3></td>
+</tr>
+<tr>
+	<td colspan="2"> Rechnungsadresse = Lieferadresse ? <input type="radio" value="ja" name="ja" checked>Ja 
+							    <input type="radio" value="nein" name="nein">Nein</td>
+</tr>
+<tr>
+	<td>Firmenname</td>
+	<td><input type="text" name="liefer_firmenname"></td>
+</tr>
+<tr>
+	<td>Vorname</td>
+	<td><input type="text" name="liefer_vorname"></td>
+</tr>
+<tr>
+	<td>Nachname</td>
+	<td><input type="text" name="nachname"></td>
+</tr>
+<tr>
+	<td>Strasse</td>
+	<td><input type="text" name="strasse"></td>
+</tr>
+<tr>
+	<td>PLZ</td>
+	<td><input type="text" name="plz"></td>
+</tr>
+<tr>
+	<td>Stadt</td>
+	<td><input type="text" name="stadt"></td>
+</tr>
+<tr>
+	<td>Land</td>
+	<td><input type="text" name=""></td>
+</tr>
+<tr>
+	<td colspan="2"><h3><center>Kontakt Informationen</center></h3></td>
+</tr>
+<tr>
+	<td>Anrede</td>
+	<td><input type="text" name="kontakt_anrede"></td>
+</tr>
+<tr>
+	<td>Titel</td>
+	<td><input type="text" name="kontakt_titel"></td>
+</tr>
+<tr>
+	<td>Vorname</td>
+	<td><input type="text" name="kontakt_vorname"></td>
+</tr>
+<tr>
+	<td>Nachname</td>
+	<td><input type="text" name="kontakt_nachname"></td>
+</tr>
+<tr>
+	<td>Telefon 1</td>
+	<td><input type="text" name="kontakt_telefon1"></td>
+</tr>
+<tr>
+	<td>Telefon 2</td>
+	<td><input type="text" name="kontakt_telefon2"></td>
+</tr>
+<tr>
+	<td>Fax-Nr.</td>
+	<td><input type="text" name="kontakt_fax"></td>
+</tr>
+<tr>
+	<td>Email</td>
+	<td><input type="text" name="kontakt_email"></td>
+</tr>
+<tr>
+	<td>Homepage</td>
+	<td><input type="text" name="kontakt_homepage"></td>
+</tr>
+<tr>
+	<td colspan="2"><h3><center>Kunden Konditionen</center></h3></td>
+</tr>
+<tr>
+	<td>Kreditlimit</td>
+	<td><input type="text" name="kreditlimit"></td>
+</tr>
+<tr>
+	<td>Zahlungsziel</td>
+	<td><input type="text" name="zahlungsziel"></td>
+</tr>
+<tr>
+	<td>Rabatt</td>
+	<td><input type="text" name="rabatt">%</td>
+</tr>
+<tr>
+	<td>Steuernummer</td>
+	<td><input type="text" name="steuernummer"></td>
+</tr>
+<tr>
+	<td>Kontonummer</td>
+	<td><input type="text" name="kontonummer"></td>
+</tr>
+<tr>
+	<td>Bankleitzahl</td>
+	<td><input type="text" name="bankleitzahl"></td>
+</tr>
+<tr>
+	<td>Bankname</td>
+	<td><input type="text" name="bankname"></td>
+</tr>
+<tr>
+	<td>Kundentyp</td>
+	<td><input type="text" name="kundentyp"></td>
+</tr>
+<tr>
+	<td>Sprache</td>
+	<td><input type="text" name="sprache"></td>
+</tr>
+<tr>
+	<td colspan="2"><center><input type="submit" value="Kunde speichern"></center></td>
+</tr>
+
+</table>
+
+</body>
+</html>
