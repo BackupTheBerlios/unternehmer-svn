ff10b794428cc83c972d1541b83c2f257a0a92dc
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-03-23 20:44:00 +0000

    menu mit ausklappbaren untermenus gebaut

--- branches/flo/menuleiste.php
+++ branches/flo/menuleiste.php
@@ -1,12 +1,26 @@
 <html>
 <body>
-<table border="1">
-<?php
-print "hallo";
-print $_POST['stamm'];
-?>
-<tr><td align="left"><img src="bilder/stammdaten.png">&nbsp;<a href="menu.php?stamm=1">Stammdaten</a><td></tr>
+<table border="0">
+
+<!-- Hier ist der Hauptmenupunkt Stammdaten definiert -->
+<tr><td align="left"><img src="bilder/stammdaten.png">&nbsp;<a href="menuleiste.php?menu_stamm=1">Stammdaten</a><td></tr>
+
+<!-- Hier ist das untermenu fuer Stammdaten definiert -->
+<?php if( isset($_GET['menu_stamm']) ) { ?>
 <tr><td align="left"><img src="bilder/menu_unterpunkt.png">&nbsp;Kunde erfassen</td><tr>
+<tr><td align="left"><img src="bilder/menu_unterpunkt.png">&nbsp;Ware erfassen</td><tr>
+<?php } ?>
+
+<!-- Hier ist der Hauptmenupunkt Verkauf definiert -->
+<tr><td align="left"><img src="bilder/menu_verkauf.png">&nbsp;<a href="menuleiste.php?menu_verkauf=1">Verkauf</a><td></tr>
+
+<!-- Hier ist das untermenu fuer Verkauf definiert
+<?php if( isset($_GET['menu_verkauf']) ) { ?>
+<tr><td align="left"><img src="bilder/menu_unterpunkt.png">&nbsp;Rechnung erfassen</td><tr>
+<?php } ?>
+
+
+
 
 </table>
 </body>
