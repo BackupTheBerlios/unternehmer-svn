454824321e25387fea31c43dd9303fcf2d4045aa
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-03-25 20:36:42 +0000

    verwaltungsbereich erweitert

--- branches/flo/verwaltungsmaske.php
+++ branches/flo/verwaltungsmaske.php
@@ -1,3 +1,41 @@
+<?php
+//auslesen der benutzer und dann die daraus tabellenreihen erstellen
+$conn = "host=localhost port=5432 dbname=phpunternehmer ".
+        "user=postgres password=";
+	
+$db = pg_connect ($conn);
+//brauche ein select statement was vorname,nachname,loginame,datenbank ausliest
+$query = "SELECT usename FROM pg_shadow";
+$resultat = pg_query($query);
+if( $resultat == false) {
+	print "fehler bei benutzer holen";
+} else {
+	for($i = 0; $i < pg_num_rows($resultat); $i++) {
+		$arr = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		
+		//user postgres auslassen
+		if($i != 0) {
+			$loginnamen[$i-1] = $arr[0];
+		}
+	}
+	//vorname und nachname holen aus go_name
+	$query = "SELECT vorname, nachname FROM go_name";
+	$resultat = pg_query($query);
+	if($resultat == false) {
+		print "fehler bei go_name";
+	}else {
+		$anzahl = pg_num_rows($resultat);
+		for($i = 0; $i < pg_num_rows($resultat); $i++) {
+			$arr = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+			$vornamen[$i] = $arr[0];
+			$nachnamen[$i] = $arr[1];
+		}
+	}
+}
+?>
+
+
+
 <html>
 <body>
 <table width="100%" border="0">
@@ -14,6 +52,18 @@
 	<td>Datenbank</td>
 </tr>
 <!-- hier kommen die flexiblen rows mit den benutzern dann automatisch -->
+<?php
+for($i = 0; $i < $anzahl; $i++) { ?>
+	<tr>
+		<td><?php print $vornamen[$i] ?></td>
+		<td><?php print $nachnamen[$i] ?></td>
+		<td><?php print $loginnamen[$i] ?></td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+<?php } ?>
+
 
 <tr>
 	<td colspan="6"><hr><td>
@@ -24,7 +74,9 @@
 	<form method="post" name="benutzererfassen" action="benutzererfassen.php">
 	<td><input type="submit" value="Benutzer erfassen" name="benutzererfassen"></td>
 	</form>
+	<form method="post" name="datenbankadministration" action="datenbankadministration.php">
 	<td><input type="submit" value="Datenbankadministration" name="datenbankadministration"></td>
+	</form>
 </tr>
 
 <!-- Anmelden an unternehmer -->
