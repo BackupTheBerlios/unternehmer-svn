0ac873936986b5e065af18a0593023a664b9c416
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-03-26 22:22:55 +0000

    ware erfassen maske erstellt 1

---
+++ branches/flo/stammdaten/ware_erfassen.php
@@ -0,0 +1,52 @@
+<html>
+<body>
+<table width="100%" border="1">
+<tr><td colspan="2"><div align="center"><h2>Ware erfassen</h2></div></td></tr>
+<tr>
+	<td>Atikelnummer</td>
+	<td>Artikelbezeichnung</td>
+</tr>
+<tr>
+	<td><input type="text" name="artikelnummer"></td>
+	<td><input type="text" name="artikelbezeichnung"></td>
+</tr>
+<tr>
+	<td align="right">Listenpreis</td>
+	<td><input type="text" name="listenpreis"></td>
+</tr>
+<tr>
+	<td align="right">Verkaufspreis</td>
+	<td><input type="text" name="verkaufspreis"></td>
+</tr>
+<tr>
+	<td align="right">Einkaufspreis</td>
+	<td><input type="text" name="einkaufspreis"></td>
+</tr>
+<tr>
+	<td align="right">Einheit</td>
+	<td><input type="text" name="einheit" value="Stck"></td>
+</tr>
+<tr>
+	<td align="right">Gewicht</td>
+	<td><input type="text" name="gewicht" value="nichtnutzbar"></td>
+</tr>
+<tr>
+	<td align="right">Mindestlagerbestand</td>
+	<td><input type="text" name="mindestlagerbestand" value="nichtnutzbar"></td>
+</tr>
+<tr>
+	<td align="right">Lagerplatz</td>
+	<td><input type="text" name="lagerplatz" value="nichtnutzbar"></td>
+</tr>
+<tr>
+	<td><h3>Konten verkn&uuml;pfen</h3></td>
+</tr>
+<tr>
+	<td>Erl&ouml;skonto</td>
+	<td><select name="erloeskont" size="0"></select></td>
+</tr>
+<tr>
+	<td>Aufwandskonto</td>
+	<td><select name="aufwandskonto" size="0"></select></td>
+</tr>
+</table>
