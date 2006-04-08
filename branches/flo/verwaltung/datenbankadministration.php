5e3cf18be2a83cf6487d6adbe1578a7f28bb8e81
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-04-08 09:00:11 +0000

    umstellung 2

---
+++ branches/flo/verwaltung/datenbankadministration.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+
+<form method="post" action="datenbankanlegen.php">
+<table width="100%" border="0">
+<tr>
+	<td colspan="2"><center><h2>Datenbankadministration</h2></center></td>
+</tr>
+<tr>
+	<td align="right">Datenbankcomputer</td>
+	<td><input type="text" name="datenbankcomputer" value="localhost"></td>
+</tr>
+<tr>
+	<td align="right">Port</td>
+	<td><input type="text" name="port" value="5432"></td>
+</tr>
+<tr>
+	<td align="right">Benutzer</td>
+	<td><input type="text" name="benutzer" value="postgres"></td>
+</tr>
+<tr>
+	<td align="right">Passwort</td>
+	<td><input type="password" name="passwort"></td>
+</tr>
+<tr>
+	<td><p></td>
+</tr>
+<tr>
+	<td><p></td>
+</tr>
+<tr>
+	<td align="right"><input type="submit" name="datenbankanlegen" value="Datenbank anlegen"></form></td>
+	<td><form method="post" action="datenbankloeschen.php"><input type="submit" name="datenbankloeschen" value="Datenbank l&ouml;schen"></form></td>
+</tr>
+</table>
+
+</body>
+</html>
---
+++ branches/flo/verwaltung/verwaltungsmaske.php
@@ -0,0 +1,107 @@
+<?php
+//auslesen der benutzer und dann die daraus tabellenreihen erstellen
+$conn = "host=localhost port=5432 dbname=phpunternehmer ".
+        "user=postgres password=";
+	
+$db = pg_connect ($conn);
+//brauche ein select statement was vorname,nachname,loginame,datenbank ausliest
+$query = "SELECT usename FROM pg_user";
+$resultat = pg_query($query);
+if( $resultat == false) {
+	print "fehler bei benutzer holen";
+} else {
+	for($i = 0; $i < pg_num_rows($resultat); $i++) {
+		$arr = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		
+		//user postgres auslassen
+		if($i != 0) {
+			$loginnamen[$i-1] = $arr[0];
+		}
+	}
+	//vorname und nachname holen aus go_name
+	$query = "SELECT vorname, nachname FROM go_name";
+	$resultat = pg_query($query);
+	if($resultat == false) {
+		print "fehler bei go_name";
+	}else {
+		$anzahl = pg_num_rows($resultat);
+		for($i = 0; $i < pg_num_rows($resultat); $i++) {
+			$arr = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+			$vornamen[$i] = $arr[0];
+			$nachnamen[$i] = $arr[1];
+		}
+	}
+}
+?>
+
+
+
+<html>
+<body>
+<table width="100%" border="0">
+<tr>
+	<td colspan="6"><center><h2>Verwaltungsmaske</h2></center></td>
+</tr>
+<!-- Benutzerabteilung, muss flexible sein, da es ja mehrere oder weniger benutzer gibt, angezeigt werden -->
+<tr>
+	<td bgcolor="grey">Vorname</td>
+	<td bgcolor="grey">Nachname</td>
+	<td bgcolor="grey">Loginname</td>
+	<td bgcolor="grey">Firma</td>
+	<td bgcolor="grey">Datenbankcomputer</td>
+	<td bgcolor="grey">Datenbank</td>
+</tr>
+<!-- hier kommen die flexiblen rows mit den benutzern dann automatisch -->
+<?php
+for($i = 0; $i < $anzahl; $i++) { ?>
+	<tr>
+		<td><?php print $vornamen[$i] ?></td>
+		<td><?php print $nachnamen[$i] ?></td>
+		<td><?php print $loginnamen[$i] ?></td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	</tr>
+<?php } ?>
+
+
+<tr>
+	<td colspan="6"><hr><td>
+</tr>
+
+<!-- Buttons -->
+<tr>
+	<form method="post" name="benutzererfassen" action="benutzererfassen.php">
+	<td align="right"><input type="submit" value="Benutzer erfassen" name="benutzererfassen"></td>
+	</form>
+	<form method="post" name="datenbankadministration" action="datenbankadministration.php">
+	<td><input type="submit" value="Datenbankadministration" name="datenbankadministration"></td>
+	</form>
+</tr>
+
+<!-- Anmelden an unternehmer -->
+<tr>
+	<td colspan="6"><hr></td>
+</tr>
+<tr>
+	<td>Direkt anmelden an "Unternehmer"</td>
+</tr>
+<form method="post" name="einloggen" action="checklogin.php">
+<tr>
+	<td align="right">Loginname</td>
+	<td><input name="loginname" type="text"></td>
+</tr>
+<tr>
+	<td align="right">Passwort</td>
+	<td><input name="passwort" type="text"></td>
+	</tr>
+<tr>
+	<td align="right"><input type="submit" value="Anmelden" name="anmelden"></td>
+</tr>
+</form>
+
+
+</table>
+</body>
+</html>
+
