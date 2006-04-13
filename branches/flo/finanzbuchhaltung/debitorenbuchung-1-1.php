32738c4c0bf733c5c72720b77ef82bad8b13c2f6
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-04-13 18:01:27 +0000

    debitorenbuchung eingecheckt

--- branches/flo/finanzbuchhaltung/debitorenbuchung-1-1.php
+++ branches/flo/finanzbuchhaltung/debitorenbuchung-1-1.php
@@ -4,37 +4,56 @@ session_start();
 $conn = "host=localhost port=5432 dbname={$_SESSION['datenbankname']} user={$_SESSION['benutzer']} password={$_SESSION['passwort']}";
 	
 $db = pg_connect($conn);
-$query = "SELECT (verkaufspreis * anzahl) from preise left outer join rechnung_vo  on(vo_preise_id=preise.id) WHERE rechnung_id={$_GET['rechnungsnr']}";
+
+//hier firmenamen,vor-und nachname holen
+$query = "SELECT firmenname, vorname, nachname FROM go_name WHERE rechnung.kunde_id = go_name.id AND rechnung.id = {$_GET['rechnungsnr']}";
 
 $resultat = pg_query($query);
+
 if($resultat == false) {
 	print "fehler";
 } else {
-	for($i = 0; $i < pg_num_rows($resultat); $i++) {
-		$array = pg_fetch_array($resultat);
-		$html_gesamtpreise[$i] = "<tr><td>&nbsp;</td><td>&nbsp;</td><td>$array[0]</td><td>&nbsp;</td></tr>";
+	$array = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+	$firmenname = $array[0];
+	$vorname = $array[1];
+	$nachname = $array[2];
+
+	$rechnungsnr = $_GET['rechnungsnr'];
+	$query = "SELECT (verkaufspreis * anzahl) from preise left outer join rechnung_vo  on(vo_preise_id=preise.id) WHERE rechnung_id='{$_GET['rechnungsnr']}'";
+
+	$resultat = pg_query($query);
+	if($resultat == false) {
+		print "fehler";
+	} else {
+		for($i = 0; $i < pg_num_rows($resultat); $i++) {
+			$array = pg_fetch_array($resultat);
+			$html_gesamtpreise[$i] = "<tr><td>&nbsp;</td><td>&nbsp;</td><td>$array[0]</td><td><input type='text' name='var_art_id'</td></tr>";
+		}
 	}
 }
-
 
 ?>
 <html>
 <body>
 <table width="100%" border="1">
+<form method="post" action="debitorenbuchung-sql.php">
 <tr>
-	<td><center><h2>Debitorenbuchung</center></h2></td>
+	<td colspan="4"><center><h2>Debitorenbuchung</center></h2></td>
 </tr>
 <tr>
-	<td>Firmenname</td>
-	<td><input type="text" name="firmenname"></td>
+	<td align="right">Firmenname</td>
+	<td colspan="3"><input type="text" name="firmenname" value="<?php echo $firmenname ?>"></td>
 </tr>
 <tr>
-	<td>Vorname</td>
-	<td><input type="text" name="vorname"></td>
+	<td align="right">Vorname</td>
+	<td colspan="3"><input type="text" name="vorname" value=<?php echo $vorname ?> ></td>
 </tr>
 <tr>
-	<td>Nachname</td>
-	<td><input type="text" name="nachname"></td>
+	<td align="right">Nachname</td>
+	<td colspan="3"><input type="text" name="nachname" value=<?php echo $nachname ?> ></td>
+</tr>
+<tr>
+	<td colspan="4">&nbsp;</td>
 </tr>
 <tr>
 	<td>Artikelnr</td>
@@ -51,7 +70,13 @@ for($i = 0; $i < count($html_gesamtpreise); $i++) {
 	print $html_gesamtpreise[$i];
 }
 ?>
-
+<tr>
+	<td colspan="4"><hr></td>
+</tr>
+<tr>
+	<td colspan="4"><center><input type="submit" name="debitorenbuchung" value="Einzahlung buchen"></center></td>
+</tr>
+</form>
 </table>
 </body>
 </html>
