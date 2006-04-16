a50970832a23e56e19c788ae520db2118239ec93
flo_k <flo_k@2d59c96e-fa0e-0410-9b4f-8a679f286d45>
2006-04-16 10:47:19 +0000

    klasse erweitert

--- branches/flo/klassen/rechnung.php
+++ branches/flo/klassen/rechnung.php
@@ -10,24 +10,59 @@ class rechnung
 	var $dbport;
 	var $dbrechner;
 	var $dbpasswort;
-
-	function nimm_dbloginname($dbloginname) {
-		$this->dbloginname = $dbloginname;
+	var $firmenname;
+	var $vorname;
+	var $nachname;
+	var $anzahlverkaufsobjekte
+	
+	//methodenauflistung , leider kann man nicht ausserhalb
+	//einer klasse die methoden definieren, so das etwas mehr uebersicht in den code 
+	//gelangt
+	//nimmDbLoginname
+	//nimmDbName
+	//nimmDbPort
+	//nimmDbRechner
+	//nimmDbPasswort
+	//nimmAlleDbDaten
+	//holeDbLoginname
+	//holeDbName
+	//holeDbPort
+	//holeDbRechner
+	//holeDbPasswort
+	//holeAlleDbDaten
+	//nimmRechnungsnr
+	//existiertRechnungsnr
+	//verbindeZuDb
+	//holeRechnungsnr
+	//ueberpruefeDbVerbindungsdaten
+	//holeFirmenname
+	//holeVorname
+	//holeNachname
+	//holeFirmennameVornameNachname
+	//holeAnzahlVerkaufsobjekte
+	//holeVerkaufsobjekt
+	
+	function nimmDbLoginname($dbloginname) {
+		$this->dbLoginname = $dbloginname;
 	}
 
-	function nimm_dbname($dbname) {
+	function nimmDbName($dbname) {
 		$this->dbname = $dbname;
 	}
 
-	function nimm_dbport($dbport) {
+	function nimmDbPort($dbport) {
 		$this->dbport = $dbport;
 	}
 
-	function nimm_dbrechner($dbrechner) {
+	function nimmDbRechner($dbrechner) {
 		$this->dbrechner = $dbrechner;
 	}
+
+	function nimmDbPasswort($dbpasswort) {
+		$this->dbpasswort = $dbpasswort;
+	}
 	
-	function nimm_alle_dbdaten($dbloginname, $dbname, $dbport, $dbrechner, $dbpasswort) {
+	function nimmAlleDbDaten($dbloginname, $dbname, $dbport, $dbrechner, $dbpasswort) {
 		$this->dbloginname = $dbloginname;
 		$this->dbname = $dbname;
 		$this->dbport = $dbport;
@@ -35,37 +70,37 @@ class rechnung
 		$this->dbpasswort = $dbpasswort;
 	}
 	
-	function hole_dbname() {
+	function holeDbName() {
 		if(!isset($this->dbname)) 
 			return -1;
 		return $this->dbname;
 	}
 
-	function hole_dbloginname() {
+	function holeDbLoginname() {
 		if(!isset($this->dbloginname))
 			return -1;
 		return $this->dbloginname;
 	}
 	
-	function hole_dbport() {
+	function holeDbPort() {
 		if(!isset($this->dbport))
 			return -1;
 		return $this->dbport;
 	}
 
-	function hole_dbrechner() {
+	function holeDbRechner() {
 		if(!isset($this->dbrechner))
 			return -1;
 		return $this->dbrechner;
 	}
 
-	function hole_dbpasswort() {
+	function holeDbPasswort() {
 		if(!isset($this->dbpasswort))
 			return -1;
 		return $this->dbpasswort;
 	}
 
-	function hole_alle_dbdaten(&$dbloginname, &$dbpasswort, &$dbport, &$dbrechner, &$dbname) {
+	function holeAlleDbDaten(&$dbloginname, &$dbpasswort, &$dbport, &$dbrechner, &$dbname) {
 		if(!isset($this->dbloginname))
 			return -1;
 		if(!isset($this->dbpasswort))
@@ -79,43 +114,51 @@ class rechnung
 
 		$dbloginname = $this->dbloginname;
 		$dbpasswort = $this->dbpasswort;
-		$dbport = $this->dbrechner;
-		$dbname = $this->dbname;
 		$dbrechner = $this->dbrechner;
+		$dbname = $this->dbname;
+		$dbport = $this->dbport;
 
 		return 0;
 	}
 
-	function nimm_rechnungsnr($rechnungsnr) {
-		if($this->ueberpruefe_db_verbindungsdaten() < 0)
+	function nimmRechnungsnr($rechnungsnr) {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
 			return -1;
-		if($this->existiert_rechnungsnr($rechnungsnr) < 0)
+		if($this->existiertRechnungsnr($rechnungsnr) < 0)
 			return -1;
 		
 		//rechnungsnr existiert
 		$this->rechnungsnummer = $rechnungsnr;
 	}
 	
-	function existiert_rechnungsnr($rechnungsnr) {
-		if($this->ueberpruefe_db_verbindungsdaten() < 0)
+	function existiertRechnungsnr($rechnungsnr) {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
 			return -1;
 		
-		if( ($dbc = $this->verbinde_zu_db()) < 0)
+		if( ($dbc = $this->verbindeZuDb()) < 0)
 			return -2;
-		
-		if(pg_query("SELECT rechnungsnr FROM rechnungen WHERE rechnungsnr='this->rechnungsnummer'") == false)
+	
+		if(!isset($rechnungsnr))
 			return -3;
+
+		$query = "SELECT id FROM rechnung WHERE id='$rechnungsnr'";
+		
+		$resultat = pg_query($query);
+		if($resultat == false)
+			return -3;
+		if(pg_num_rows($resultat) <= 0)
+			return -4;
 
 		return 0;
 	}
 
-	function verbinde_zu_db() {
-		if($this->ueberpruefe_db_verbindungsdaten() < 0)
+	function verbindeZuDb() {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
 			return -1;
 		
-		$conn = "host=this->dbrechner port=this->dbport dbname=this->dbname ".
-			"user=this->dbloginname password=this->passwort";
-	
+		$conn = "host={$this->dbrechner} port={$this->dbport} dbname={$this->dbname} ".
+			"user={$this->dbloginname} password={$this->dbpasswort}";
+
 		$dbc = pg_connect($conn);
 
 		return $dbc;
@@ -124,14 +167,14 @@ class rechnung
 	
 		
 	
-	function hole_rechnungsnr() {
+	function holeRechnungsnr() {
 		if(!isset($this->rechnungsnummer))
 			return -1;
 
 		return $this->rechnungsnummer;
 	}
 
-	function ueberpruefe_db_verbindungsdaten() {
+	function ueberpruefeDbVerbindungsdaten() {
 		if(!isset($this->dbloginname)) 
 			return -1;
 		if(!isset($this->dbname))
@@ -146,7 +189,116 @@ class rechnung
 		return 0;
 	}
 	
+	function holeFirmenname() {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
+			return -1;
+		if($this->existiertRechnungsnr($this->rechnungsnummer) < 0)
+			return -2;
+		if( ($db = $this->verbindeZuDb()) < 0)
+			return -3;
 		
+		$resultat = pg_query("SELECT firmenname FROM  go_name g ,rechnung r WHERE ".
+					"r.id=$this->rechnungsnummer AND g.id=r.kunde_id");
+		if($resultat == false)
+			return -4;
+
+		$resultat1 = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		$this->firmenname = $resultat1[0];
+		
+		return $this->firmenname;
+	}
+	
+	function holeVorname() {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
+			return -1;
+		if($this->existiertRechnungsnr($this->rechnungsnummer) < 0)
+			return -2;
+		if( ($db = $this->verbindeZuDb()) < 0)
+			return -3;
+		
+		$resultat = pg_query("SELECT vorname FROM  go_name g ,rechnung r WHERE ".
+					"r.id=$this->rechnungsnummer AND g.id=r.kunde_id");
+		if($resultat == false)
+			return -4;
+
+		$resultat1 = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		$this->vorname = $resultat1[0];
+		
+		return $this->vorname;
+	}
+	
+	function holeNachname() {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
+			return -1;
+		if($this->existiertRechnungsnr($this->rechnungsnummer) < 0)
+			return -2;
+		if( ($db = $this->verbindeZuDb()) < 0)
+			return -3;
+		
+		$resultat = pg_query("SELECT nachname FROM  go_name g ,rechnung r WHERE ".
+					"r.id=$this->rechnungsnummer AND g.id=r.kunde_id");
+		if($resultat == false)
+			return -4;
+
+		$resultat1 = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		$this->nachname = $resultat1[0];
+		
+		return $this->nachname;
+	}
+
+	function holeFirmennameVornameNachname(&$firmenname, &$vorname, &$nachname) {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
+			return -1;
+		if($this->existiertRechnungsnr($this->rechnungsnummer) < 0)
+			return -2;
+		if( ($db = $this->verbindeZuDb()) < 0)
+			return -3;
+		
+		$resultat = pg_query("SELECT firmenname, vorname, nachname FROM  go_name g ,rechnung r WHERE ".
+					"r.id=$this->rechnungsnummer AND g.id=r.kunde_id");
+		if($resultat == false)
+			return -4;
+
+		$resultat1 = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		$firmenname = $this->firmenname = $resultat1[0];
+		$vorname = $this->vorname = $resultat1[1];
+		$nachname = $this->nachname = $resultat1[2];
+		
+		return 0;
+	}
+
+	function holeAnzahlVerkaufsobjekte() {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
+			return -1;
+		if($this->existiertRechnungsnr($this->rechnungsnummer) < 0)
+			return -2;
+		if( ($db = $this->verbindeZuDb()) < 0)
+			return -3;
+		
+		$resultat = pg_query("SELECT count(*) FROM rechnung_vo WHERE ".
+					"rechnung_id=$this->rechnungsnummer");
+
+		if($resultat == false)
+			return -4;
+		$resultat1 = pg_fetch_array($resultat, NULL, PGSQL_NUM);
+		$this->anzahlverkaufsobjekte = $resultat1[0];
+
+		return $this->anzahlverkaufsobjekte;
+	}
+	
+	function holeVerkaufsobjekt($nr) {
+		if($this->ueberpruefeDbVerbindungsdaten() < 0)
+			return -1;
+		if($this->existiertRechnungsnr($this->rechnungsnummer) < 0)
+			return -2;
+		if(!isset($this->anzahlverkaufsobjekte))
+			return -3;
+		if($nr > $this->anzahlverkaufsobjekte)
+			return -4;
+		if( ($db = $this->verbindeZuDb()) < 0)
+			return -3;
+		
+
 }		
 
 ?>
